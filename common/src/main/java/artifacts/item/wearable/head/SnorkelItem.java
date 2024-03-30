9f722b9841e412e24a2586e88d246d614540a712
Wouter Pauwels <37985539+ochotonida@users.noreply.github.com>
2024-03-30 23:29:40 +0100

    Add more accurate snorkel tooltip, remove unused lang key

--- common/src/main/java/artifacts/item/wearable/head/SnorkelItem.java
+++ common/src/main/java/artifacts/item/wearable/head/SnorkelItem.java
@@ -2,17 +2,30 @@ package artifacts.item.wearable.head;
 
 import artifacts.item.wearable.MobEffectItem;
 import artifacts.registry.ModGameRules;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.tags.FluidTags;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+
+import java.util.List;
 
 public class SnorkelItem extends MobEffectItem {
 
     public SnorkelItem() {
         super(MobEffects.WATER_BREATHING, ModGameRules.SNORKEL_ENABLED);
+    }
+
+    @Override
+    protected void addEffectsTooltip(ItemStack stack, List<MutableComponent> tooltip) {
+        if (ModGameRules.SNORKEL_WATER_BREATHING_DURATION.get() == 0) {
+            tooltip.add(tooltipLine("infinite"));
+        } else {
+            tooltip.add(tooltipLine("limited"));
+        }
     }
 
     @Override
