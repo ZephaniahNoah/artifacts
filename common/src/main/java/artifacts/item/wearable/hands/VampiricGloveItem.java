b5bbc43843d4de65dd0237653a6f4e89943ba031
Wouter Pauwels <37985539+ochotonida@users.noreply.github.com>
2024-04-03 19:12:16 +0200

    Add more accurate vampiric glove tooltip

--- common/src/main/java/artifacts/item/wearable/hands/VampiricGloveItem.java
+++ common/src/main/java/artifacts/item/wearable/hands/VampiricGloveItem.java
@@ -4,8 +4,12 @@ import artifacts.item.wearable.WearableArtifactItem;
 import artifacts.registry.ModGameRules;
 import artifacts.registry.ModItems;
 import artifacts.util.DamageSourceHelper;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.List;
 
 public class VampiricGloveItem extends WearableArtifactItem {
 
@@ -14,6 +18,15 @@ public class VampiricGloveItem extends WearableArtifactItem {
         return ModGameRules.VAMPIRIC_GLOVE_ABSORPTION_RATIO.get() == 0
                 || ModGameRules.VAMPIRIC_GLOVE_MAX_HEALING_PER_HIT.get() == 0
                 || ModGameRules.VAMPIRIC_GLOVE_ABSORPTION_CHANCE.get() == 0;
+    }
+
+    @Override
+    protected void addEffectsTooltip(ItemStack stack, List<MutableComponent> tooltip) {
+        if (ModGameRules.VAMPIRIC_GLOVE_ABSORPTION_CHANCE.fuzzyEquals(1)) {
+            tooltip.add(tooltipLine("constant"));
+        } else {
+            tooltip.add(tooltipLine("chance"));
+        }
     }
 
     public static void onLivingDamage(LivingEntity entity, DamageSource damageSource, float amount) {
